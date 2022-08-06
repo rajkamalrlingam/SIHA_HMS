5fd3d919534654581c35fdaa8cd88c3f61c9fc49
Rajkamal R <Rajkamal R@RAJKAMAL>
2022-08-06 23:10:25 +0530

    chat bot changes

--- siha_hms/src/main/resources/static/doctor/vendor/lightbox2/dist/js/lightbox-plus-jquery.js
+++ siha_hms/src/main/resources/static/doctor/vendor/lightbox2/dist/js/lightbox-plus-jquery.js
@@ -10259,7 +10259,7 @@ return jQuery;
  * More info:
  * http://lokeshdhakar.com/projects/lightbox2/
  *
- * Copyright 2007, 2018 Lokesh Dhakar
+ * Copyright 2007, 2022 Lokesh Dhakar
  * Released under the MIT license
  * https://github.com/lokesh/lightbox2/blob/master/LICENSE
  *
